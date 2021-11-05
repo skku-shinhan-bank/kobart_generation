9c948366dbfb298d8afabfd6e2fd9789e20e1565
Hans-0101 <hyhsumin@naver.com>
2021-11-05 12:08:35 +0900

    feat:kobart 모듈화 ver10

--- kobart_generation/module/__init__.py
+++ kobart_generation/module/__init__.py
@@ -1,3 +1,3 @@
-# from .dataset import ChatDataModule, ChatDataset
-# from .model import Base
+from .dataset import ChatDataModule, ChatDataset
+from .model import Base
 # from .trainer import ArgsBase, KoBARTConditionalGeneration
