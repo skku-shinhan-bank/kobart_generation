80145bf5376e4f70891ea6718980f9e398359d6e
Hans-0101 <hyhsumin@naver.com>
2021-11-08 16:40:33 +0900

    feat:ver27

--- kobart_generation/__init__.py
+++ kobart_generation/__init__.py
@@ -1,3 +1,4 @@
 from .model import Base
 from .dataset import CommentDataset, CommentDataModule
 from .trainer import ArgsBase, KoBARTConditionalGeneration
+from .generator import KoBARTCommentGenerator
