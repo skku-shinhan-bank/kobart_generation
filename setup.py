8ca02b984f5bfd5f91a0525a0455b7804330b685
Hans-0101 <hyhsumin@naver.com>
2021-11-11 03:07:09 +0900

    feat:readme 수정

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 with open('requirements.txt', "r", encoding="utf-8") as f:
     requirements = f.read().splitlines()
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name="skku-shinhan-bank/kobart_generation",
@@ -12,8 +12,8 @@ setup(
     author="SKKU Shinhan Bank",
     author_email="ajtwlswjddnv1102@gmail.com",
     description="kobart generation",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     url="https://github.com/skku-shinhan-bank/kobart_generation",
     packages=find_packages(),
     install_requires=requirements,
