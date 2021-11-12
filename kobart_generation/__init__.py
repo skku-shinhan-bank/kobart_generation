cfd8819ea8ca54efef422c5d5aa0c03b4a921cb8
Hans-0101 <hyhsumin@naver.com>
2021-11-12 12:56:57 +0900

    feat:trainer init 추가

--- kobart_generation/__init__.py
+++ kobart_generation/__init__.py
@@ -1,4 +1,4 @@
 from .model import Base, KoBARTGenerationModel
 from .dataset import CommentDataset, CommentDataModule
-from .trainer import ArgsBase
+from .trainer import ArgsBase, KoBARTGenerationTrainer
 from .generator import KoBARTCommentGenerator
