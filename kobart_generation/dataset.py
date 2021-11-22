54c1a85c113fe33fcbe9be130dea4dd695c0660b
Hans-0101 <hyhsumin@naver.com>
2021-11-22 16:13:29 +0900

    feat:token 확인

--- kobart_generation/dataset.py
+++ kobart_generation/dataset.py
@@ -46,10 +46,6 @@ class CommentDataset(Dataset):
             self.tokenizer.tokenize(str(id)+' '+q) + [self.eos_token]
         a_tokens = [self.bos_token] + \
             self.tokenizer.tokenize(a) + [self.eos_token]
-
-        print(q_tokens)
-        print(a_tokens)
-
         encoder_input_id, encoder_attention_mask = self.make_input_id_mask(
             q_tokens, index)
         decoder_input_id, decoder_attention_mask = self.make_input_id_mask(
